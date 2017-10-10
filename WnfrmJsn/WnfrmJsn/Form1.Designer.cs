615e301c7b2ce613014c160bf3895819efcf8ec3
D <ydimco@gmail.com>
2017-10-10 14:59:35 +0300

    Add the cosmetic changes

--- WnfrmJsn/WnfrmJsn/Form1.Designer.cs
+++ WnfrmJsn/WnfrmJsn/Form1.Designer.cs
@@ -32,18 +32,19 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblCity = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(82, 144);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(107, 22);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(333, 32);
+            this.textBox2.Location = new System.Drawing.Point(333, 53);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(296, 151);
@@ -51,27 +52,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 321);
+            this.button1.Location = new System.Drawing.Point(82, 261);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(107, 36);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Get Info";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(333, 219);
+            this.textBox3.Location = new System.Drawing.Point(333, 261);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(296, 151);
             this.textBox3.TabIndex = 3;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(79, 108);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(91, 17);
+            this.lblCity.TabIndex = 4;
+            this.lblCity.Text = "Enter the city";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 502);
+            this.Controls.Add(this.lblCity);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -89,6 +100,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblCity;
     }
 }
 
